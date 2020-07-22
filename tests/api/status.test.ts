368c97ddc681b45da63dbb6e6f28caf6aafa481f
shimyounghyun <lhlrol1004@naver.com>
2020-07-22 23:02:09 +0900

    CI fail test

--- tests/api/status.test.ts
+++ tests/api/status.test.ts
@@ -6,6 +6,6 @@ describe("GET /api/auth/status", ()=> {
    it("200: 연결 성공", async () => {
       const response = await request(app)
           .get("/api/auth/status")
-          .expect(200);
+          .expect(400);
    });
 });
